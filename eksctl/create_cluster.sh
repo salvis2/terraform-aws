0a52e2549f61fd7bcbf019020ac7d580fbcc75a1
Sebastian Alvis <sebialvis@gmail.com>
2020-01-07 16:00:11 -0800

    Changed profile name to what the eks folder sets up. Also put arguments in the same order on both lines.

--- eksctl/create_cluster.sh
+++ eksctl/create_cluster.sh
@@ -4,7 +4,7 @@
 set -e
 
 # Check if cluster exists first
-eksctl create cluster --profile eksbot --config-file=eksctl-config.yml
+eksctl create cluster --profile eksctlbot --config-file=eksctl-config.yml
 
 # Return valid JSON string
-eksctl get cluster --profile eksbot --name jupyterhub -o json | jq -r ".[] | {Arn:.Arn}"
+eksctl get cluster --profile eksctlbot --name jupyterhub -o json | jq -r ".[] | {Arn:.Arn}"
